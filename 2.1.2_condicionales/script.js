0473e7afaebe47fa9c90817a4d1e009dea7fa028
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-05-24 19:31:22 -0500

    Agregar el promptt

---
+++ 2.1.2_condicionales/script.js
@@ -0,0 +1,6 @@
+//let nombre; -> undefined un valor no controlado que no existe
+//let nombre = null; porque estoy dejando sin saber de que tipo seria la variable
+let nombre = "";
+console.log(`El valor de nombre antes del prompt: ${nombre}.`)
+nombre = prompt("Ingresa tu nombre");
+console.log(`El nombre ingresado es: ${nombre}.`)
