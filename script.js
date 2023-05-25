873a2f2ca921afd6940e574d129b96e25fefded2
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-05-24 19:05:51 -0500

    repaso js: consolo.log, alert y document.write

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 // Inserción de codigo --> DOM --> Página
 document.write("Hola mundo x2");
 
-document.write("<h1>Bienvenidos a el repaso de JS</h1>")
+//  "" , '' , ` (alt + 96)
+
+// document.write('<h1 style="color:red;">Bienvenidos a el repaso de JS</h1>')
+document.write('<h1>Bienvenidos a el repaso de JS</h1>')
+
 console.log("Hola Mundo!");
+
+console.log()
+
+alert("Hola mundo")
