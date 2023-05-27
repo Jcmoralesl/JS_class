c6e13ddcdc271c8936bb197ce308117410b656b4
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-05-26 20:58:48 -0500

    Ejercicio 6 JC

--- 2.1.2_condicionales/script.js
+++ 2.1.2_condicionales/script.js
@@ -64,7 +64,7 @@ if (respuesta === "si") {
 
 // Segundo ejercicio: Solicitar al usuario un número, y determinar si es divisible entre dos o no. Mostrando al usuario un mensaje de “x número es divisible entre 2” o “x núm"
 
-let n1 = prompt (`Escribe un numero`)
+let n1 = prompt (`Escribe un numero`);
 if (n1 % 2 === 0) {
     console.log (" "+n1+" es divisible entre 2");
 } else {
@@ -73,7 +73,7 @@ if (n1 % 2 === 0) {
 
 // Tercer ejercicio: Crear un programa que determine si un número introducido en un Prompt es par o no, la respuesta será mostrada en un Alert. 
 
-let n2 = prompt (`Escribe un numero`)
+let n2 = prompt (`Escribe un numero`);
 if (n2%2 === 0) {
     alert ("El numero "+n2+" es par");
 } else {
@@ -82,20 +82,39 @@ if (n2%2 === 0) {
 
 // Cuarto ejercicio: Solicitar al usuario un número de cliente. Si el número es el 1000, imprimir "Ganaste un premio", en caso contrario mostrar el número y el mensaje “Lo sentimos, sigue participando”.
 
-let n3 = prompt (`Escribe un numero`)
+let n3 = prompt (`Escribe un numero`);
 if (n3 == 1000) {
-    alert ("Ganaste un premio")
+    alert ("Ganaste un premio");
 } else {
-    alert (" "+n3+" : Lo sentimos, sigue participando ")
+    alert (" "+n3+" : Lo sentimos, sigue participando ");
 }
 
 // Solicitar al usuario que ingrese dos números y mostrar cuál de los dos es menor. No considerar el caso en que ambos números son iguales.
 
-let n4 = prompt (`Escribe un numero`) 
-let n5 = prompt (`Escribe otro numero`)
+let n4 = prompt (`Escribe un numero`);
+let n5 = prompt (`Escribe otro numero`);
 
 if (n4 < n5) {
-    document.write ("El numero "+n4+" es el menor")
+    document.write ("El numero "+n4+" es el menor");
 } if (n5 < n4) {
-    document.write ("El numero "+n5+" es el menor")
+    document.write ("El numero "+n5+" es el menor");
 }
+
+// Solicitar al usuario que ingrese tres números y mostrar cuál de los tres es el número mayor. Considerar el caso en que 2 números sean iguales.
+
+let n6 = prompt (`Escribe un numero`);
+let n7 = prompt (`Escribe otro numero`);
+let n8 = prompt (`EScribe un ultimo numero`);
+
+if  (n6 > n7 && n6 > n8) {
+    document.write ("El numero "+n6+" es el mayor ");
+} else if (n7 > n8) {
+    document.write ("El numero "+n7+" es el mayor ");
+} else if (n6 == n7 && n6 == n8) {
+    document.write ("Los numeros son iguales");
+} else {
+    document.write ("El numero "+n8+" es el mayor ")
+}
+
+// Requerir al usuario que ingrese un día de la semana e imprimir un mensaje si es lunes, otro mensaje diferente si es viernes, otro mensaje diferente si es sábado o domingo. Si el día ingresado no es ninguno de esos, imprimir otro mensaje.
+
