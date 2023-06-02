7241115cb22d636a03a9976d9b41068e166e451c
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-06-02 17:01:40 -0500

    Ejercicio 1, While

---
+++ 2.2.1_estructuras_control/ejercicios_while.js
@@ -0,0 +1,11 @@
+/* console.log ("Eyy mundo") */ //Prueba de script 
+
+// EJERCICIOS WHILE 
+
+// EJERCICIO 1: Crea un programa que pregunte al usuario un número. Mostrar los números que son múltiplos de 5 desde 1 hasta el número introducido por el usuario.
+
+let p1 = prompt ("Escribe un número")
+
+while (p1 % 5) {
+    console.log ()
+}
