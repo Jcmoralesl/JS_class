7b2b0d55ca83070425b46e128e33a978f59d684d
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-06-04 23:25:16 -0500

    Ejercicio 1, for JS

---
+++ 2.2.1_estructuras_control/ejercicios_for.js
@@ -0,0 +1,13 @@
+/* console.log ("¡Hello guys!") */ //Confirmación script/html
+
+// EJERCIOS FOR 
+
+// EJERCICIO 1: Crea un programa que imprima en consola los números impares del 1 al 50.
+
+function numerosImpares () {
+    for (let index = 1; index <= 50; index += 2) {
+        console.log (index);
+    }
+}
+
+numerosImpares ();
