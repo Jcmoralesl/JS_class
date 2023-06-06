8384025c33869769a05be07e91c3ccecad244e13
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-06-05 19:32:45 -0500

    Empezamos funciones JS

--- 2.2.1_estructuras_control/ejercicios_for.js
+++ 2.2.1_estructuras_control/ejercicios_for.js
@@ -30,7 +30,7 @@ for (let index = 0; index < pokemon.length && index < x; index +=5) {
 
 // EJERCICIO 3: Crea un programa que recorra el arreglo [4,”dos”,8,”tres”,5,9,1,”cero”] y muestre en consola solo los elementos que son tipo número.
 
-let y = [4 ,'dos', 8, `tres`, 5, 9, 1, `cero`];
+const y = [4 ,'dos', 8, `tres`, 5, 9, 1, `cero`];
 
 function soloNumeros(array) {
     let soloenteros = [];
