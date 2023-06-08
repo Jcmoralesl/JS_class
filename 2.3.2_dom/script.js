de18cb2a3d654059ba1dc44abfbff72060a3fd4f
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-06-07 21:47:14 -0500

    DOM + Node/JS

--- 2.3.2_dom/script.js
+++ 2.3.2_dom/script.js
@@ -29,3 +29,32 @@ function saludar1 () {
 
 // DOM = Document Object Model 
 
+
+// GET ELEMENT: Obetener elementos 
+
+// Para obtener elementos ID
+
+const parrafo1 = document.getElementById(`parrafo1`)
+const parrafo2 = document.getElementById(`parrafo2`)
+
+console.log (parrafo1, parrafo2)
+
+// Para obtener elementos por etiqueta 
+
+const button = document.getElementsByName("button")
+console.log (button)
+
+// Para obtener elementos por clase 
+
+const parrafo3 = document.getElementsByClassName("long")
+console.log (parrafo3)
+
+// querySelector: Puede funcionar como los 3 anteriores 
+// para ID = usamos el # + el nombre del ID 
+// para etiquetas = entre " " nombre de la etiqueta. nos devuelve el primero que encuentra 
+// para una clase = . + nombre de la clase, devuelve el primero que encuentra con esa clase 
+
+// querySelectorAll: devuelve todos los elementos que concidan con el selector 
+
+
+// Como podemos convertir un note.list a un array 
