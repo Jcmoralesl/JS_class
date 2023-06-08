0a2e849aacccbde7951711e21881bf24a14eb840
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-06-07 19:40:12 -0500

    Aplicando la función saludar al HTML/JS

--- 2.3.2_dom/script.js
+++ 2.3.2_dom/script.js
@@ -1,1 +1,31 @@
-const alumnosMali = ["Yinedth", "Steven", "Sharon", "Sebastian", "David"]
+const alumnosMali = ["Yinedth", "Steven", "Sharon", "Sebastian", "David"];
+
+const alumnoSpan = document.querySelector(`#alumno`);
+
+let index = 0;
+
+// Recorrido circular de un arreglo
+/**
+ * index % tamaño del arreglo 
+ * 0 % 5 == 0
+ * ...
+ * ...
+ * ...
+ * 6 % 5 == 1
+ * 6 % 5 == 2
+ * 7 % 5 == 3
+ */
+
+setInterval(saludar, 1500);
+
+function saludar () {
+    alumnoSpan.innerHTML = alumnosMali[index % alumnosMali.length]
+    index++
+};
+
+function saludar1 () {
+    alert ("Hola mundo")
+}
+
+// DOM = Document Object Model 
+
