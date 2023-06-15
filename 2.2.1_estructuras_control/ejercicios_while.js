79653306229e0dbf3921e4bfc47fcd9fe5c017c7
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-06-14 19:42:37 -0500

    Ejercicio 5 While/JS

--- 2.2.1_estructuras_control/ejercicios_while.js
+++ 2.2.1_estructuras_control/ejercicios_while.js
@@ -52,7 +52,7 @@ while (true) {
 
 // EJERCICIO 4: Crea un programa que solicite al usuario letras o palabras, si es así guardar el resultado. Para terminar de capturar el usuario no debe escribir valor alguno. Al terminar de capturar, mostrar en pantalla la concatenación de todas las palabras capturadas.
 
-const y = []
+/* const y = []
 let x = parseInt(prompt ("Escriba una letra o palabra"))
 
 while (x !== "") {
@@ -64,4 +64,27 @@ while (x !== "") {
     x=prompt("Escriba una letra o palabra, si no quieres ingresar mas numeros deja vacio")
 }
 
-console.log (y);
+console.log (y); */
+
+// EJERCICIO 5:  Crea un programa que solicite al usuario un día de la semana (ej: lunes, jueves, domingo, etc). El programa mostrará un mensaje personalizado para cada día de la semana por medio de un alert. Y seguirá pidiendo al usuario introducir otro día. En caso de que el día introducido sea domingo mostrar al usuario el mensaje “Ve a descansar” y terminar la captura de información.
+
+let dia = prompt ("Ingrese un día de la semana") 
+
+while (dia !== "domingo") {
+    dia=prompt ("Ingrese un día de la semana")
+    if (dia === "lunes") {
+        alert ("Estamos comenzando la semana, vamos con toda")
+    } else if (dia === "martes") {
+        alert ("Un día mas de la semana, todavía tenemos fuerzas")
+    } else if (dia === "miercoles") {
+        alert ("Mitad de semana, se empieza a sentir el cansancio, pero no nos rindamos todavía")
+    } else if (dia === "jueves") {
+        alert ("Un dia vamos ya casi termina esta semana laboral, ¡animo!")
+    } else if (dia === "viernes") {
+        alert ("¡LO LOGRASTE!, que hay pa ser")
+    } else if (dia === "sabado"){
+        alert ("No se que paso ayer pero la pase bueno")
+    } else {
+        alert ("Ve a descansar")
+    }
+}
