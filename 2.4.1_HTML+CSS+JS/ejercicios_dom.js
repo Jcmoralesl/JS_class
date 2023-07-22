4f77b24b005eb13dedea90b8ec14a84eaac65a6f
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-07-21 22:02:06 -0500

    Entrega Dom

--- 2.4.1_HTML+CSS+JS/ejercicios_dom.js
+++ 2.4.1_HTML+CSS+JS/ejercicios_dom.js
@@ -1,1 +1,29 @@
+const baseDeDatos = [
+    {
+        id: 1,
+        nombre: 'Aqua',
+        precio: 200,
+    },
+    {
+        id: 2,
+        nombre: 'Emocion',
+        precio: 180,
+    },
+    {
+        id: 3,
+        nombre: 'Alegria',
+        precio: 160,
+    },
+    {
+        id: 4,
+        nombre: 'Frescura',
+        precio: 150,
+    }
+
+];
+
+
+let carrito = [];
+const DOMañadir = document.querySelector(`#añadirPerfume`)
+const DOMresultado = document.querySelector(`#resultadoVendedores`)
 
