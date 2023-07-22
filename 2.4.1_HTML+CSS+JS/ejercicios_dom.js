677b77b15ecdbbeecc17ccf7b5c56c02e45b07eb
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-07-22 13:16:11 -0500

    Ejercicio DOM- 20%

--- 2.4.1_HTML+CSS+JS/ejercicios_dom.js
+++ 2.4.1_HTML+CSS+JS/ejercicios_dom.js
@@ -23,7 +23,66 @@ const baseDeDatos = [
 ];
 
 
-let carrito = [];
-const DOMañadir = document.querySelector(`#añadirPerfume`)
-const DOMresultado = document.querySelector(`#resultadoVendedores`)
+let carritoJuana = [];
+let carritoPedro = [];
+
+const DOMañadir = document.querySelector(`#añadirPerfume`);
+const DOMresultado = document.querySelector(`#resultadoVendedores`);
+// const DOMvendedor = document.getElementById(`nombreVendedor`).value;
+// const DOMperfume = document.getElementById(`nombreProducto`).value;
+// const DOMcantidad = document.getElementById(`numero`).value; 
+
+// function ShowSelected() {
+/* Para obtener el valor */
+
+// const DOMvendedor = document.getElementById(`nombreVendedor`).value;
+// console.log (DOMvendedor)
+// const DOMperfume = document.getElementById(`nombreProducto`).value;
+// console.log (DOMperfume)
+// const DOMcantidad = document.getElementById(`numero`).value;
+// console.log (DOMcantidad)
+
+/* Para obtener el texto */
+
+//     const DOMvendedor = document.getElementById(`nombreVendedor`);
+//     const ven = DOMvendedor.options [DOMvendedor.selectedIndex].text;
+
+//     const DOMperfume = document.getElementById(`nombreProducto`);
+//     const perf = DOMperfume.options [DOMperfume.selectedIndex].text;
+
+//     const DOMcantidad = document.getElementById(`numero`);
+//     const cant = DOMcantidad.options [DOMcantidad.selectedIndex].text;
+
+//     DOMañadir.addEventListener(`click`, function(){ 
+//         console.log(ven)
+//         console.log(perf)
+//         console.log(cant)
+//     });
+
+// }
+
+const DOMvendedor = document.getElementById(`nombreVendedor`)
+DOMvendedor.addEventListener(`change` , function () {
+    var selectedOption = this.options [DOMvendedor.selectedIndex];
+})
+
+const DOMperfume = document.getElementById(`nombreProducto`);
+DOMperfume.addEventListener (`change`,function (){
+    var selectedOption = this.options [DOMperfume.selectedIndex];
+})
+
+const DOMcantidad = document.getElementById(`numero`);
+DOMcantidad.addEventListener (`change`,function (){
+    var selectedOption = this.options [DOMcantidad.selectedIndex];
+})
+
+DOMañadir.addEventListener(`click`, function(){ 
+    console.log(DOMvendedor.value)
+    console.log(DOMperfume.value)
+    console.log(DOMcantidad.value)
+    console.log(carritoJuana)
+    console.log(carritoPedro)
+
+
+});
 
