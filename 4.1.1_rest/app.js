2fd73b72c3764710bbde11e5df5f2ec4bfc77762
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-07-26 21:00:24 -0500

    First servidor: Hola mundo

--- 4.1.1_rest/app.js
+++ 4.1.1_rest/app.js
@@ -1,1 +1,14 @@
-console.log('hola');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+// app.get para obtener la informacion
+app.get('/', (req, res) => {
+    res.send('Hola hermoso mundo!')
+});
+
+//crea un puerto en mi computadora 
+app.listen(port, () => {
+    console.log(`Aplicacion de ejemplo en el puerto: ${port}`)
+});
