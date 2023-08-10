9e4ca35d2d75f5ed14cfb54f23f3664c684a03b4
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-08-09 20:36:20 -0500

    Driver - Conexion con database

--- 4.2.1_conexion_database/main.js
+++ 4.2.1_conexion_database/main.js
@@ -10,3 +10,25 @@ const pool = new pg.Pool({
     password:'root',
 });
 
+pool.query(`SELECT * from actor WHERE first_name='Penelope'`, (error,results) => {
+    if (error) {
+        console.error('Ocurrio un error');
+        console.error (error);
+        return;
+    }
+    console.log(results.rows)
+})
+
+const persona = {
+    name: 'Juan',
+    apellido: 'Morales',
+}
+
+pool.query (`INSERT INTO actor(first_name, last_name) VALUES ($1, $2)`, [persona.name, persona.apellido],(error,results) => {
+    if (error) {
+        console.error('Ocurrio un error');
+        console.error (error);
+        return;
+    }
+    console.log(JSON.stringify(results));
+})
