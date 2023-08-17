687edebcbbc006802df7b91cef37e09e92282034
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-08-16 20:11:04 -0500

    Modelo y Controladores de clientes

---
+++ 4.4.1_backend/models/client.js
@@ -0,0 +1,36 @@
+const connection = require("../knexfile")['development'];
+const database = require('knex')(connection);
+
+const getAllClients = () => {
+    return database('clients');
+};
+
+const getClient = (idClient) => {
+    return database('clients')
+        .where( { id: idClient });
+};
+
+const insertClient = (client) => {
+    return database('clients')
+        .insert(client);
+};
+
+const updateClient = (idClient, client) => {
+    return database ('clients')
+        .where('id', '=', idClient)
+        .update(client);
+    }
+
+const deleteClient = (idClient) => {
+    return database('clients')
+        .where ('id', '=', idClient)
+        .del ()  
+}
+
+module.exports = {
+    getAllClients,
+    getClient,
+    insertClient,
+    updateClient,
+    deleteClient
+}
--- 4.4.1_backend/routes/clientRoutes.js
+++ 4.4.1_backend/routes/clientRoutes.js
@@ -4,7 +4,22 @@ const router = express.Router();
 
 const { clientController } = require('./../controllers');
 
+// obtenemos todos los clientes
 router.get('/', clientController.getClients);
+
+// insertamos un solo cliente
+router.post('/', clientController.insertClient)
+
+//actualizar un cliente
+router.put('/:id', clientController.updateClient)
+
+// eliminar un cliente 
+router.delete('/:id', clientController.deleteClient)
+
+// obtener un solo cliente
+router.get('/:id', clientController.getClient)
+
+
 
 
 module.exports = router;
