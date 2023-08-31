119b39945de2c3485a7f523073677aaf0940aa71
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-08-30 19:34:04 -0500

    Conexión con el servidor de MongoDB Atlas

--- 4.6.1_no-sql/server.js
+++ 4.6.1_no-sql/server.js
@@ -6,6 +6,17 @@ const server = express()
 
 server.use(express.json())
 
+const mongooseConnect = async () => {
+    try{
+        await mongoose.connect('mongodb+srv://juancamilomorales43:wio82jPQvGjcfcSh@cluster0.abtu2od.mongodb.net/')
+        console.log('Conexión exitosa')
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+mongooseConnect ()
+
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
