60b933bf9d62b022b480c0f92f26c27d172ca96e
Jcmoralesl <juancamilo.morales43@gmail.com>
2023-08-30 21:30:52 -0500

    Modelo y backend completo MongoDB

--- 4.6.1_no-sql/models/Task.js
+++ 4.6.1_no-sql/models/Task.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose')
+
 const taskSchema = {
     title: {
         type: String,
@@ -15,3 +17,32 @@ const taskSchema = {
     } 
     
 }
+
+const Task = mongoose.model('Task', taskSchema)
+
+const getAll = async () => {
+    const result = await Task.find()
+    return  result;
+}
+
+const insertTask = async (task) => {
+    return await Task.insertOne(task) 
+}
+
+const updateTask = async (id, newTask) => {
+    const oldTask = await Task.findById(id)
+    oldTask.title = newTask.title
+    oldTask.description = newTask.description
+    oldTask.deadline= newTask.deadline
+    oldTask.done = newTask.done
+    return await oldTask .save();
+}
+
+const removeTask = async (id) => {
+    return await Task.deleteOne({id})
+}
+
+const getTask = async (id) => {
+    return   await Task.findById (id);
+}
+module.exports={getAll,getTask,removeTask,updateTask,insertTask};
